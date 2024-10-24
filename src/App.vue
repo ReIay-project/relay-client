051312a8fa9133c9b2ac59cdd3abc4b0ff6a61d3
Kotaro <swdevstudio@gmail.com>
2024-10-25 00:05:25 +0500

    CLIENT-36: Add storybook and example

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,21 @@
 <template>
-  <main />
+  <main>
+    <StorybookExample
+      title="te"
+      :item="{ name: '' }"
+      :list="[]"
+    />
+  </main>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue'; 
 import { invoke } from '@tauri-apps/api/core';
+import StorybookExample from './components/examples/StorybookExample/StorybookExample.vue';
 
 export default defineComponent({
   name: 'App',
+  components: { StorybookExample },
   data: () => ({
     greetMsg: '',
     name: ''
